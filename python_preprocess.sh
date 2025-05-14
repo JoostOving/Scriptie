a38b24ecf4f322d4982ca54ea4daeb535ec1c71d
JoostOving <124837513+JoostOving@users.noreply.github.com>
2025-05-14 18:20:12 +0200

    Update python_preprocess.sh

--- python_preprocess.sh
+++ python_preprocess.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=00:05:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --mem=20000
+#SBATCH --output=preprocess_output.log
+#SBATCH --error=preprocess_error.log
 module load Python/3.10.4-GCCcore-11.3.0
-module load CUDA/11.7.0
-module load Boost/1.79.0-GCC-11.3.0
-python ./kopie_van_scriptie_code.py
+python ./preprocess_data.py
