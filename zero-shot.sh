882e4e0752a1680d973bbe7dcbe8bbcb982e9cee
JoostOving <124837513+JoostOving@users.noreply.github.com>
2025-06-07 13:20:59 +0200

    Update zero-shot.sh

--- zero-shot.sh
+++ zero-shot.sh
@@ -10,20 +10,16 @@
 
 VENV_PATH="/home3/s5251370/venvs/first_env"
 
-# Load modules
 module load Python/3.10.4-GCCcore-11.3.0
 module load CUDA/11.7.0
 module load Boost/1.79.0-GCC-11.3.0
 
+# statements added so the correct virtual environment is selected and multiple GPU's are activated
 source "$VENV_PATH/bin/activate"
-
 export CUDA_VISIBLE_DEVICES=0,1
-
 export HF_HOME=/scratch/$USER/huggingface_cache
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-
-
-# Run your script
-python ./eval_tester.py
+# the file you want to run
+python ./zero-shot.py
 
